a4b17e6a01aa8857a19f582227b685c5075a2598
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-02-25 23:03:24 +0800

    - Add config "shortname" on app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,9 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel x Stisla'),
+
+    'shortname' => env('APP_SHORTNAME', 'LS'),
 
     /*
     |--------------------------------------------------------------------------
