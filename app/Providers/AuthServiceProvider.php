ab4837d55e96caf7033240ce4f6e73ae08f8bb51
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-02-26 17:04:39 +0800

    - Add policy class for user model - Create gate definition on auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Infrastructure\Contracts\Auth\HasRole;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +27,28 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $this->defineGate();
+    }
+
+    /**
+     * Set gate policy definition.
+     *
+     * @return void
+     */
+    protected function defineGate()
+    {
+        Gate::before(function (HasRole $user, $ability, array $models) {
+            if ($user->isAdmin()) {
+                if (in_array($ability, ['delete', 'forceDelete'])) {
+                    $model = head($models);
+
+                    if ($model instanceof Model && $model->is($user)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        });
     }
 }
