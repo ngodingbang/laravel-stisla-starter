f4411e3dc03018308c2779c03ca8395dc16c15a5
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2022-04-02 09:28:46 +0700

    - Change HOME value on RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard-ecommerce';
+    public const HOME = '/dashboard/ecommerce';
 
     /**
      * {@inheritDoc}
